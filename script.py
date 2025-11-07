c9bbf491ed9850fac52aa48ef8f64d23c90fa41e
engrron30 <ronaldtorresrosete30@gmail.com>
2025-11-08 01:51:55 +0800

    Normal traffic variable added

--- script.py
+++ script.py
@@ -8,6 +8,8 @@ demo_vid_file_type = "mp4"
 demo_vid_dir = "Sample Data"
 if DEMO_ERROR_VIDEO:
     demo_vid_name = f"vid-with-malicious-traffic.{demo_vid_file_type}"
+else:
+    demo_vid_name = f"vid-with-normal-traffic.{demo_vid_file_type}"
 
 # Credentials and Other Details to Fill-Up Livestream URL
 user_name = "hwjk"
